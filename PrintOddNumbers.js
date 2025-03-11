3185a6bd6c86fdbee97850bcf83ff38c7a274ef1
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-03-11 16:14:29 +0530

    Print Odd  Numbers within 1 to 10
    This code prints only the odd numbers from 1 to 10.
    It uses a for loop that counts from 1 to 10. Inside the loop, an if statement checks if the number is odd. The condition j % 2 == 1 tests if a number divided by 2 leaves a remainder of 1, which is true for odd numbers.

---
+++ PrintOddNumbers.js
@@ -0,0 +1,22 @@
+//Print only odd numbers within 1 to 10
+console.log("Print only odd numbers:");
+
+for(var j=1; j<=10; j++)
+{
+ 	if(j % 2 == 1)  // If you want to print even numbers use  -> if(j % 2 == 0) 
+	{
+		console.log(j);
+	}
+}
+
+/* Output:
+	 
+	Print only odd numbers:
+	1
+	3
+	5
+	7
+	9
+
+*/
+
