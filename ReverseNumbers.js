ac046ae9071c9fa027055ed5b9769dfa7f3a3062
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-03-11 16:31:22 +0530

    Print the numbers in reverse
    This code uses a for loop that starts with i=10, runs as long as i is greater than or equal to 1, and decreases i by 1 each time through the loop using the decrement operator (i--). Inside the loop, it prints the current value of i to the console.

---
+++ ReverseNumbers.js
@@ -0,0 +1,22 @@
+//Print the numbers in reverse
+console.log("Print the numbers in reverse: ")
+
+for(var i=10; i>=1; i--)
+{
+	console.log(i);
+}
+
+/* 	Output: 
+
+	Print the numbers in reverse:
+	10
+	9
+	8
+	7
+	6
+	5
+	4
+	3
+	2	
+	1
+*/
