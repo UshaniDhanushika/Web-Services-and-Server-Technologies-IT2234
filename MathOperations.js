66346974df74525584ce7e07c22d6a4f0cb7172a
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-03-11 15:53:32 +0530

    Basic Math Operations
    This code demonstrates basic math operations in JavaScript using both let and var variable declarations.
    
    The main difference between let and var variables is that with var, we can redeclare the same variables (ex: var Num2 = 2 is redeclared as var Num2 = 3). In the output, the value of Num2 is 3. But, with let we cannot declare variables with the same name. It is best to use let in coding because it is better than var for preventing errors.

---
+++ MathOperations.js
@@ -0,0 +1,65 @@
+//Basic Mathematical Operations 
+
+console.log("Mathematical operations:")
+console.log(" ")
+
+
+console.log(" ---- Using let ---- " )
+
+let num1 = 10
+let num2 = 5
+
+console.log("Number 1: " + num1)
+console.log("Number 2: " + num2)
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+let mod = num1 % num2;
+
+
+console.log("Summation is: " + sum )
+console.log("Substraction is: " + sub )
+console.log("Multiplication is: " + mul )
+console.log("Division is: " + div )
+console.log("Modules is: " + mod )
+console.log(" ")
+
+
+console.log(" ---- Using var ---- " )
+
+var Num1 = 12
+var Num2 = 2
+var Num2 = 3
+
+console.log("Number 1: " + Num1)
+console.log("Number 2: " + Num2)
+
+
+var add = Num1 + Num2 + Num2
+var subs = Num1 - Num2 
+
+console.log("Addition is: " + add )
+console.log("Substraction is: " + subs )
+
+/* Output:
+
+	Mathematical operations:
+
+	 ---- Using let ----
+	Number 1: 10
+	Number 2: 5
+	Summation is: 15
+	Substraction is: 5
+	Multiplication is: 50
+	Division is: 2
+	Modules is: 0
+
+	 ---- Using var ----
+	Number 1: 12
+	Number 2: 3
+	Addition is: 18
+	Substraction is: 9
+
+*/
