4a6065db27739184c22af95a4d22dccd6ee7a6ff
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-03-17 22:44:51 +0530

    JSON
    This code shows how to work with JSON objects in JavaScript. It first defines a single student's details. Then, it creates an array of multiple student objects and displaying their information

---
+++ JSON.js
@@ -0,0 +1,16 @@
+// JSON
+//{key:value}
+
+let student = {regno: '2021/ICT/01', name: 'James', age:21, course: 'IT', skills: ['java', "JS", 'c++']}
+console.log(student)
+console.log(student.name)
+
+let students = [
+	{regno: '2021/ICT/01', name: 'james', age:21, course: 'IT'},
+	{regno: '2021/ICT/02', name: 'ravi', age:22, course: 'AMC'},
+	{regno: '2021/ICT/03', name: 'gaya', age:23, course: 'Bio'}
+
+]
+
+console.log(students)
+
