40a9ebcf5cb8793f280b6469d1d38f193d46f918
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-03-19 16:35:54 +0530

    Boolean function to find a given number is prime
    The isPrime function checks whether a number is prime. It returns false for numbers less than 2 or when the number is divisible by any integer between 2 and itself and returns true otherwise. In this example, isPrime(5) outputs true (since 5 is prime), and isPrime(24) outputs false (because 24 is divisible by 2, among others).

---
+++ booleanFunction.js
@@ -0,0 +1,21 @@
+//Write a boolean function to find a given number is prime 
+
+
+function isPrime(num) {
+   if(num < 2)
+   {
+	   return false;
+   }
+   
+   for(let i = 2; i<num; i++)
+   {
+	   if(num % i == 0)
+		   return false
+	   
+   }
+   
+   return true;
+}
+
+console.log(isPrime(5))
+console.log(isPrime(24))
