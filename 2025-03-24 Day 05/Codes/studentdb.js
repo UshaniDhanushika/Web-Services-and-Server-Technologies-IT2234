4f541a78a4cb17bfd23a7dc85ff34199dd3875a8
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-03-24 20:59:07 +0530

    Add files via upload

---
+++ 2025-03-24 Day 05/Codes/studentdb.js
@@ -0,0 +1,10 @@
+//array of student JSON details
+
+let students = [
+    {regno:'2021ict01',name:'Nethya',gender:'F'},
+    {regno:'2021ict02',name:'Ramesha',gender:'F'},
+    {regno:'2021ict03',name:'Elsa',gender:'F'},
+    {regno:'2021ict04',name:'Kethaka',gender:'M'}
+    ];
+
+    module.exports=students;
